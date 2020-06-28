8d54ce06559ef7be50e1e236579582e6a791429c
Cleber <contato.cleberrezende@gmail.com>
2020-06-28 17:44:49 -0300

    Pequenos ajustes

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,3 +2,5 @@ export default {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
 };
+
+// docker run --name redis -p 6379:6379 -d -t redis:alpine
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-    dsn: process.env.SENTRY_DNS,
+    dsn: process.env.SENTRY_DSN,
 };
