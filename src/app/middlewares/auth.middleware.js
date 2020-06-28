0e88f1a89e77c18d2754fabb63a1e6c41deb472c
Cleber <contato.cleberrezende@gmail.com>
2020-06-28 16:44:16 -0300

    Listagem de horários disponíveis

--- src/app/middlewares/auth.middleware.js
+++ src/app/middlewares/auth.middleware.js
@@ -9,7 +9,7 @@ export default async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader)
-        return res.status(401).json({ error: 'Token not provided' });
+        return res.status(400).json({ error: 'Token not provided' });
 
     const [, token] = authHeader.split(' ');
 
